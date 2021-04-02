36c05f4cec7947c65b20baab5495595ccd676bd7
Олег Шмелев <oleshm@gmail.com>
2021-04-02 16:48:42 +0400

    Исправлен пропуск первого года в списке статей блога

--- get_links.R
+++ get_links.R
@@ -5,7 +5,7 @@ args <-
   commandArgs(trailingOnly = TRUE)
 
 if (length(args) != 1) {
-  stop("Blog name must be supplied (input something like oldadmiral).n", call.=FALSE)
+  stop("Blog name must be supplied (input something like oldadmiral)", call.=FALSE)
 }
 
 library(tidyverse)
@@ -19,31 +19,35 @@ get_links <-
       str_subset(pattern)
   }
 
-link_years <-
+href <- 
   read_html(str_c("https://", args[1], ".livejournal.com/calendar")) %>% 
-  html_elements("a") %>% 
-  html_attr("href") %>% 
+  html_elements("a") %>%
+  html_attr("href")
+
+link_years <- 
+  href %>% 
   str_subset(str_c("^https://", args[1], ".livejournal.com/[:digit:]{4}/$"))
 
 link_month <- 
-  link_years %>%
-  map(get_links, pattern = str_c("^https://", args[1], ".livejournal.com/[:digit:]{4}/[:digit:]{2}/$")) %>%
-  flatten_chr()
+  c(
+    href %>% 
+      str_subset(str_c("^https://", args[1], ".livejournal.com/[:digit:]{4}/[:digit:]{2}/$")), 
+    link_years %>%
+      map(get_links, pattern = str_c("^https://", args[1], ".livejournal.com/[:digit:]{4}/[:digit:]{2}/$")) %>%
+      flatten_chr()
+  )
 
-# http://www.livejournal.com/view/?type=month&format=light&user=oldadmiral&y=2017&m=03
-
-link_articles <-
-  str_c(
-    "http://www.livejournal.com/view/?type=month&format=light&user=", 
-    args[1], "&y=", 
-    link_month %>% 
-      unique() %>% 
-      str_extract("[:digit:]{4}/[:digit:]{2}") %>% 
-      str_replace("/", "&m=")
-  ) %>%
+link_articles <- 
+  link_month %>% 
   map(get_links, pattern = str_c("^https://", args[1], ".livejournal.com/[:digit:]+\\.html$")) %>%
   flatten_chr()
 
-link_articles %>% 
-  sort() %>% unique() %>% 
+link_articles <- 
+  tibble(
+  link = link_articles,
+  itemid = str_replace(link, ".*/([:digit:]*)\\.html", "\\1") %>% as.integer()
+) %>% 
+  arrange(itemid)
+
+link_articles$link %>%
   write_lines(str_c(args[1], ".txt"))
